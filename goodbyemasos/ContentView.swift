c6421c3d2e4144767f6233151ac31de331f2a372
typachill <typachillx@yandex.ru>
2026-06-29 18:14:39 +0300

    Initial Commit

---
+++ goodbyemasos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  goodbyemasos
+//
+//  Created by Кирилл Чивикин on 29.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ goodbyemasos/goodbyemasosApp.swift
@@ -0,0 +1,17 @@
+//
+//  goodbyemasosApp.swift
+//  goodbyemasos
+//
+//  Created by Кирилл Чивикин on 29.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct goodbyemasosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
